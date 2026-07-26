ee0eda8bb0ec5de7d92b624dea7fe4116387b0c1
Anupam Katiyar <anupam934@gmail.com>
2026-07-26 16:00:18 +0530

    fix api base url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,15 @@
 import axios from "axios";
 
+console.log("API URL =>", import.meta.env.VITE_API_URL);
+
 const API = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true,
+});
+
+API.interceptors.request.use((config) => {
+  console.log("Request URL =>", `${config.baseURL}${config.url}`);
+  return config;
 });
 
 export default API;
