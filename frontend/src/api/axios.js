ff6980d4a35fb5da553bd0f81da85faaff53a4b5
Anupam Katiyar <anupam934@gmail.com>
2026-08-03 22:43:32 +0530

    Fix CORS origin

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,13 +3,21 @@ import axios from "axios";
 console.log("API URL =>", import.meta.env.VITE_API_URL);
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
-  withCredentials: true,
+    baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 API.interceptors.request.use((config) => {
-  console.log("Request URL =>", `${config.baseURL}${config.url}`);
-  return config;
+
+    const token = localStorage.getItem("token");
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    console.log("Request URL =>", `${config.baseURL}${config.url}`);
+    console.log("Token =>", token);
+
+    return config;
 });
 
 export default API;
