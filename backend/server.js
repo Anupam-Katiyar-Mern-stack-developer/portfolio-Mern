93774ec4aa75da3b265b6bc0a4d6a674d1505e96
Anupam Katiyar <anupam934@gmail.com>
2026-08-03 21:55:04 +0530

    Fix CORS origin

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       
-      "https://glittery-phoenix-14e9f9.netlify.app",
+      "https://anupamportfolio01.netlify.app/x-14e9f9.netlify.app",
       "https://portfolio-frontend.vercel.app"
     ],
     methods: ["GET", "POST"],
